08d89a6736015bb8bbeae08e1e85bb0fd281b530
tubakoc <tubakoc@users.noreply.github.com>
2022-04-28 13:45:17 +0300

    added Splash Screen

---
+++ app/src/main/java/com/example/gezirehberim/view/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.example.gezirehberim.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gezirehberim.R
+import com.example.gezirehberim.view.activity.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //splash ekran için bir Thread oluşturuyoruz
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    //threaad 5 sn yani 5000 ms uyusun
+                    Thread.sleep(2500)
+                    //intent ile splash ekranından sonra MainActivity ekranı açılsın diyoruz
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
